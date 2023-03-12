769d1625f41244c38429d25871505a813ce4b5c8
Samuel Colvin <s@muelcolvin.com>
2023-03-12 14:44:36 +0000

    changing benchmarks to use a vec input

--- benches/main.rs
+++ benches/main.rs
@@ -9,26 +9,45 @@ use pyo3::types::{PyList, PyTuple};
 
 use rust_bench::{PyListBuilder, PyTupleBuilder, list_as_tuple};
 
-fn run_py_list_builder(py: Python, capacity: usize, to_push: usize) -> PyResult<&PyList> {
-    let mut list_builder = PyListBuilder::with_capacity(py, capacity)?;
-    for i in 0..to_push {
-        list_builder.push(py , i)?;
+fn get_value(i: &usize) -> String {
+    format!("value_{}", i)
+    // *i
+}
+
+fn run_py_list_builder<'py>(py: Python<'py>, input: &[usize]) -> PyResult<&'py PyList> {
+    let mut list_builder = PyListBuilder::with_capacity(py, input.len())?;
+    for i in input {
+        list_builder.push(py , get_value(i))?;
     }
     list_builder.get(py)
 }
 
-fn run_py_list_builder_incomplete(py: Python, capacity: usize, to_push: usize) -> PyResult<&PyList> {
-    let mut list_builder = PyListBuilder::with_capacity(py, capacity)?;
-    for i in 0..to_push {
-        list_builder.push(py , i)?;
+fn run_py_list_builder_incomplete<'py>(py: Python<'py>, break_at: usize, input: &[usize]) -> PyResult<&'py PyList> {
+    let mut list_builder = PyListBuilder::with_capacity(py, input.len())?;
+    for i in input {
+        list_builder.push(py , get_value(i))?;
+        if i >= &break_at {
+            break;
+        }
     }
     Ok(list_builder.get_incomplete(py))
 }
 
-fn run_py_list_vec(py: Python, capacity: usize, to_push: usize) -> &PyList {
-    let mut vec = Vec::with_capacity(capacity);
-    for i in 0..to_push {
-        vec.push(i);
+fn run_py_list_vec<'py>(py: Python<'py>, input: &[usize]) -> &'py PyList {
+    let mut vec = Vec::with_capacity(input.len());
+    for i in input {
+        vec.push(get_value(i));
+    }
+    PyList::new(py, vec)
+}
+
+fn run_py_list_vec_incomplete<'py>(py: Python<'py>, break_at: usize, input: &[usize]) -> &'py PyList {
+    let mut vec = Vec::with_capacity(input.len());
+    for i in input {
+        vec.push(get_value(i));
+        if i >= &break_at {
+            break;
+        }
     }
     PyList::new(py, vec)
 }
@@ -36,13 +55,16 @@ fn run_py_list_vec(py: Python, capacity: usize, to_push: usize) -> &PyList {
 #[bench]
 fn py_list_complete_builder(bench: &mut Bencher) {
     Python::with_gil(|py| -> PyResult<()> {
-        let list_5 = run_py_list_builder(py, 5, 5)?;
-        let list_5_expected = run_py_list_vec(py, 5, 5);
+        let vec_5 = vec![0, 1, 2, 3, 4];
+        let list_5 = run_py_list_builder(py, &vec_5)?;
+        let list_5_expected = run_py_list_vec(py, &vec_5);
         assert!(list_5.eq(list_5_expected)?);
 
+        let vec_500: Vec<usize> = (0..500).collect();
+
         bench.iter(|| {
-            let list_50 = run_py_list_builder(py, 50, 50).unwrap();
-            black_box(list_50);
+            let list_500 = run_py_list_builder(py, black_box(&vec_500)).unwrap();
+            black_box(list_500);
         });
         Ok(())
     }).unwrap();
@@ -51,8 +73,10 @@ fn py_list_complete_builder(bench: &mut Bencher) {
 #[bench]
 fn py_list_complete_vec(bench: &mut Bencher) {
     Python::with_gil(|py| {
+        let vec_500: Vec<usize> = (0..500).collect();
+
         bench.iter(|| {
-            let py_list = run_py_list_vec(py, 50, 50);
+            let py_list = run_py_list_vec(py, black_box(&vec_500));
             black_box(py_list);
         });
     });
@@ -62,12 +86,14 @@ fn py_list_complete_vec(bench: &mut Bencher) {
 #[bench]
 fn py_list_incomplete_builder(bench: &mut Bencher) {
     Python::with_gil(|py| -> PyResult<()> {
-        let list_5 = run_py_list_builder_incomplete(py, 10, 5)?;
-        let list_5_expected = run_py_list_vec(py, 5, 5);
-        assert!(list_5.eq(list_5_expected)?);
+        let vec_5 = vec![0, 1, 2, 3, 4];
+        let list_3 = run_py_list_builder_incomplete(py, 3, &vec_5)?;
+        let list_3_expected = run_py_list_vec_incomplete(py, 3, &vec_5);
+        assert!(list_3.eq(list_3_expected)?);
 
+        let vec_500: Vec<usize> = (0..500).collect();
         bench.iter(|| {
-            let list_40 = run_py_list_builder_incomplete(py, 60, 50).unwrap();
+            let list_40 = run_py_list_builder_incomplete(py, black_box(400), black_box(&vec_500)).unwrap();
             black_box(list_40);
         });
         Ok(())
@@ -78,8 +104,9 @@ fn py_list_incomplete_builder(bench: &mut Bencher) {
 #[bench]
 fn py_list_incomplete_vec(bench: &mut Bencher) {
     Python::with_gil(|py| {
+        let vec_500: Vec<usize> = (0..500).collect();
         bench.iter(|| {
-            let py_list = run_py_list_vec(py, 60, 50);
+            let py_list = run_py_list_vec_incomplete(py, black_box(400), black_box(&vec_500));
             black_box(py_list);
         });
     });
@@ -87,26 +114,40 @@ fn py_list_incomplete_vec(bench: &mut Bencher) {
 
 ////////////////////////////
 
-fn run_py_tuple_builder(py: Python, capacity: usize, to_push: usize) -> PyResult<&PyTuple> {
-    let mut tuple_builder = PyTupleBuilder::with_capacity(py, capacity)?;
-    for i in 0..to_push {
-        tuple_builder.push(py, i)?;
+fn run_py_tuple_builder<'py>(py: Python<'py>, input: &[usize]) -> PyResult<&'py PyTuple> {
+    let mut tuple_builder = PyTupleBuilder::with_capacity(py, input.len())?;
+    for i in input {
+        tuple_builder.push(py, get_value(i))?;
     }
     tuple_builder.get(py)
 }
 
-fn run_py_tuple_builder_incomplete(py: Python, capacity: usize, to_push: usize) -> PyResult<&PyTuple> {
-    let mut tuple_builder = PyTupleBuilder::with_capacity(py, capacity)?;
-    for i in 0..to_push {
-        tuple_builder.push(py, i)?;
+fn run_py_tuple_builder_incomplete<'py>(py: Python<'py>, break_at: usize, input: &[usize]) -> PyResult<&'py PyTuple> {
+    let mut tuple_builder = PyTupleBuilder::with_capacity(py, input.len())?;
+    for i in input {
+        tuple_builder.push(py, get_value(i))?;
+        if i >= &break_at {
+            break;
+        }
     }
     Ok(tuple_builder.get_incomplete(py))
 }
 
-fn run_py_tuple_vec(py: Python, capacity: usize, to_push: usize) -> &PyTuple {
-    let mut vec = Vec::with_capacity(capacity);
-    for i in 0..to_push {
-        vec.push(i);
+fn run_py_tuple_vec<'py>(py: Python<'py>, input: &[usize]) -> &'py PyTuple {
+    let mut vec = Vec::with_capacity(input.len());
+    for i in input {
+        vec.push(get_value(i));
+    }
+    PyTuple::new(py, vec)
+}
+
+fn run_py_tuple_vec_incomplete<'py>(py: Python<'py>, break_at: usize, input: &[usize]) -> &'py PyTuple {
+    let mut vec = Vec::with_capacity(input.len());
+    for i in input {
+        vec.push(get_value(i));
+        if i >= &break_at {
+            break;
+        }
     }
     PyTuple::new(py, vec)
 }
@@ -114,13 +155,15 @@ fn run_py_tuple_vec(py: Python, capacity: usize, to_push: usize) -> &PyTuple {
 #[bench]
 fn py_tuple_complete_builder(bench: &mut Bencher) {
     Python::with_gil(|py| -> PyResult<()> {
-        let tuple_5 = run_py_tuple_builder(py, 5, 5)?;
-        let tuple_5_expected = run_py_tuple_vec(py, 5, 5);
+        let vec_5 = vec![0, 1, 2, 3, 4];
+        let tuple_5 = run_py_tuple_builder(py, &vec_5)?;
+        let tuple_5_expected = run_py_tuple_vec(py, &vec_5);
         assert!(tuple_5.eq(tuple_5_expected)?);
 
+        let vec_500: Vec<usize> = (0..500).collect();
         bench.iter(|| {
-            let tuple_50 = run_py_tuple_builder(py, 50, 50).unwrap();
-            black_box(tuple_50);
+            let tuple_500 = run_py_tuple_builder(py, black_box(&vec_500)).unwrap();
+            black_box(tuple_500);
         });
         Ok(())
     }).unwrap();
@@ -129,8 +172,9 @@ fn py_tuple_complete_builder(bench: &mut Bencher) {
 #[bench]
 fn py_tuple_complete_vec(bench: &mut Bencher) {
     Python::with_gil(|py| {
+        let vec_500: Vec<usize> = (0..500).collect();
         bench.iter(|| {
-            let py_tuple = run_py_tuple_vec(py, 50, 50);
+            let py_tuple = run_py_tuple_vec(py, black_box(&vec_500));
             black_box(py_tuple);
         });
     });
@@ -140,12 +184,14 @@ fn py_tuple_complete_vec(bench: &mut Bencher) {
 #[bench]
 fn py_tuple_incomplete_builder(bench: &mut Bencher) {
     Python::with_gil(|py| -> PyResult<()> {
-        let tuple_5 = run_py_tuple_builder_incomplete(py, 10, 5)?;
-        let tuple_5_expected = run_py_tuple_vec(py, 5, 5);
-        assert!(tuple_5.eq(tuple_5_expected)?);
+        let vec_5 = vec![0, 1, 2, 3, 4];
+        let tuple_3 = run_py_tuple_builder_incomplete(py, 3, &vec_5)?;
+        let tuple_3_expected = run_py_tuple_vec_incomplete(py, 3, &vec_5);
+        assert!(tuple_3.eq(tuple_3_expected)?);
 
+        let vec_500: Vec<usize> = (0..500).collect();
         bench.iter(|| {
-            let tuple_40 = run_py_tuple_builder_incomplete(py, 60, 50).unwrap();
+            let tuple_40 = run_py_tuple_builder_incomplete(py, black_box(400), black_box(&vec_500)).unwrap();
             black_box(tuple_40);
         });
         Ok(())
@@ -155,8 +201,9 @@ fn py_tuple_incomplete_builder(bench: &mut Bencher) {
 #[bench]
 fn py_tuple_incomplete_vec(bench: &mut Bencher) {
     Python::with_gil(|py| {
+        let vec_500: Vec<usize> = (0..500).collect();
         bench.iter(|| {
-            let py_tuple = run_py_tuple_vec(py, 60, 50);
+            let py_tuple = run_py_tuple_vec_incomplete(py, black_box(400), black_box(&vec_500));
             black_box(py_tuple);
         });
     });
@@ -165,15 +212,17 @@ fn py_tuple_incomplete_vec(bench: &mut Bencher) {
 #[bench]
 fn list_as_tuple_direct(bench: &mut Bencher) {
     Python::with_gil(|py| {
-        let py_list_5 = run_py_list_vec(py, 5, 5);
-        let py_tuple_expected = run_py_tuple_vec(py, 5, 5);
+        let vec_5 = vec![0, 1, 2, 3, 4];
+        let py_list_5 = run_py_list_vec(py, &vec_5);
+        let py_tuple_expected = run_py_tuple_vec(py, &vec_5);
         let py_tuple_5 = list_as_tuple(py, py_list_5);
         assert!(py_tuple_5.eq(py_tuple_expected).unwrap());
 
-        let py_list_50 = run_py_list_vec(py, 50, 50);
+        let vec_500: Vec<usize> = (0..500).collect();
+        let py_list_500 = run_py_list_vec(py, &vec_500);
 
         bench.iter(|| {
-            let py_tuple = list_as_tuple(py, black_box(py_list_50));
+            let py_tuple = list_as_tuple(py, black_box(py_list_500));
             black_box(py_tuple);
         });
     });
@@ -182,10 +231,11 @@ fn list_as_tuple_direct(bench: &mut Bencher) {
 #[bench]
 fn list_as_tuple_iterate(bench: &mut Bencher) {
     Python::with_gil(|py| {
-        let py_list_50 = run_py_list_vec(py, 50, 50);
+        let vec_500: Vec<usize> = (0..500).collect();
+        let py_list_500 = run_py_list_vec(py, &vec_500);
 
         bench.iter(|| {
-            let py_tuple = PyTuple::new(py, py_list_50);
+            let py_tuple = PyTuple::new(py, py_list_500);
             black_box(py_tuple);
         });
     });
--- src/lib.rs
+++ src/lib.rs
@@ -3,38 +3,55 @@ use std::cmp::Ordering;
 use pyo3::{AsPyPointer};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
-use pyo3::ffi::{Py_ssize_t, PyList_AsTuple, PyList_GetSlice, PyList_New, PyList_SetItem, PyTuple_GetSlice, PyTuple_New, PyTuple_SetItem};
+use pyo3::ffi;
 use pyo3::types::{PyList, PyTuple};
 
 
 pub struct PyListBuilder {
-    len: Py_ssize_t,
+    len: ffi::Py_ssize_t,
     list: Py<PyList>,
-    counter: Py_ssize_t,
+    counter: ffi::Py_ssize_t,
 }
 
 impl PyListBuilder {
     pub fn with_capacity(py: Python, capacity: usize) -> PyResult<Self> {
-        let len: Py_ssize_t = capacity
+        let len: ffi::Py_ssize_t = capacity
             .try_into()
             .map_err(|_| PyValueError::new_err("list len out of range"))?;
         unsafe {
-            let ptr = PyList_New(len);
+            let ptr = ffi::PyList_New(len);
             let list: Py<PyList> = Py::from_owned_ptr(py, ptr);
             Ok(Self { len, list, counter: 0 })
         }
     }
 
-    pub fn push(&mut self, py: Python, item: impl ToPyObject) -> PyResult<()>{
+    pub fn push(&mut self, py: Python, item: impl ToPyObject) -> PyResult<()> {
         let ptr = self.list.as_ptr();
         unsafe {
-            match PyList_SetItem(ptr, self.counter, item.to_object(py).into_ptr()) {
+            match ffi::PyList_SetItem(ptr, self.counter, item.to_object(py).into_ptr()) {
                 0 => {
                     self.counter += 1;
                     Ok(())
                 }
                 _ => Err(PyValueError::new_err("push() exceeded list capacity")),
             }
+        }
+    }
+
+    pub fn push_alt(&mut self, py: Python, item: impl ToPyObject) -> PyResult<()> {
+        if self.counter == self.len {
+            Err(PyValueError::new_err("push() exceeded list capacity"))
+        } else {
+            unsafe {
+                let obj_ptr = item.to_object(py).into_ptr();
+
+                #[cfg(not(Py_LIMITED_API))]
+                ffi::PyList_SET_ITEM(self.list.as_ptr(), self.counter, obj_ptr);
+                #[cfg(Py_LIMITED_API)]
+                ffi::PyList_SetItem(self.list.as_ptr(), self.counter, obj_ptr);
+            }
+            self.counter += 1;
+            Ok(())
         }
     }
 
@@ -56,7 +73,7 @@ impl PyListBuilder {
             // we haven't yet filled the list, return a slice
             Ordering::Less => unsafe {
                 let ptr = self.list.as_ptr();
-                let slice_ptr = PyList_GetSlice(ptr, 0, self.counter);
+                let slice_ptr = ffi::PyList_GetSlice(ptr, 0, self.counter);
                 let py_list: Py<PyList> = Py::from_owned_ptr(py, slice_ptr);
                 py_list.into_ref(py)
             },
@@ -69,25 +86,25 @@ impl PyListBuilder {
 pub fn list_as_tuple<'py>(py: Python<'py>, list: &'py PyList) -> &'py PyTuple {
     let py_tuple: Py<PyTuple> = unsafe {
         let ptr = list.as_ptr();
-        let tuple_ptr = PyList_AsTuple(ptr);
+        let tuple_ptr = ffi::PyList_AsTuple(ptr);
         Py::from_owned_ptr(py, tuple_ptr)
     };
     py_tuple.into_ref(py)
 }
 
 pub struct PyTupleBuilder {
-    len: Py_ssize_t,
+    len: ffi::Py_ssize_t,
     tuple: Py<PyTuple>,
-    counter: Py_ssize_t,
+    counter: ffi::Py_ssize_t,
 }
 
 impl PyTupleBuilder {
     pub fn with_capacity(py: Python, capacity: usize) -> PyResult<Self> {
-        let len: Py_ssize_t = capacity
+        let len: ffi::Py_ssize_t = capacity
             .try_into()
             .map_err(|_| PyValueError::new_err("tuple len out of range"))?;
         unsafe {
-            let ptr = PyTuple_New(len);
+            let ptr = ffi::PyTuple_New(len);
             let tuple: Py<PyTuple> = Py::from_owned_ptr(py, ptr);
             Ok(Self { len, tuple, counter: 0 })
         }
@@ -96,7 +113,7 @@ impl PyTupleBuilder {
     pub fn push(&mut self, py: Python, item: impl ToPyObject) -> PyResult<()> {
         let ptr = self.tuple.as_ptr();
         unsafe {
-            match PyTuple_SetItem(ptr, self.counter, item.to_object(py).into_ptr()) {
+            match ffi::PyTuple_SetItem(ptr, self.counter, item.to_object(py).into_ptr()) {
                 0 => {
                     self.counter += 1;
                     Ok(())
@@ -123,7 +140,7 @@ impl PyTupleBuilder {
             // we haven't yet filled the tuple, return a slice
             Ordering::Less => unsafe {
                 let ptr = self.tuple.as_ptr();
-                let slice_ptr = PyTuple_GetSlice(ptr, 0, self.counter);
+                let slice_ptr = ffi::PyTuple_GetSlice(ptr, 0, self.counter);
                 let py_tuple: Py<PyTuple> = Py::from_owned_ptr(py, slice_ptr);
                 py_tuple.into_ref(py)
             },
