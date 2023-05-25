5b63ffcffb78eec6a06beaf8748cf164dac9e34d
Samuel Colvin <s@muelcolvin.com>
2023-05-25 19:06:00 +0100

    add extract methods

--- benches/main.rs
+++ benches/main.rs
@@ -337,3 +337,113 @@ fn iter_list_iter(bench: &mut Bencher) {
         });
     });
 }
+
+#[bench]
+fn extract_str_extract_success(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let s = PyString::new(py, "Hello, World!") as &PyAny;
+
+        bench.iter(|| {
+            let v = black_box(s).extract::<&str>().unwrap();
+            black_box(v);
+        });
+    });
+}
+
+#[bench]
+fn extract_str_extract_fail(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let d = py.None().into_ref(py);
+
+        bench.iter(|| {
+            match black_box(d).extract::<&str>() {
+                Ok(v) => panic!("should err {}", v),
+                Err(e) => black_box(e),
+            }
+        });
+    });
+}
+
+#[bench]
+fn extract_str_downcast_success(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let s = PyString::new(py, "Hello, World!") as &PyAny;
+
+        bench.iter(|| {
+            let py_str = black_box(s).downcast::<PyString>().unwrap();
+            let v = py_str.to_str().unwrap();
+            black_box(v);
+        });
+    });
+}
+
+#[bench]
+fn extract_str_downcast_fail(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let d = py.None().into_ref(py);
+
+        bench.iter(|| {
+            match black_box(d).downcast::<PyString>() {
+                Ok(v) => panic!("should err {}", v),
+                Err(e) => black_box(e),
+            }
+        });
+    });
+}
+
+#[bench]
+fn extract_int_extract_success(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let int_obj: PyObject = 123.into_py(py);
+        let int = int_obj.as_ref(py);
+
+        bench.iter(|| {
+            let v = black_box(int).extract::<i64>().unwrap();
+            black_box(v);
+        });
+    });
+}
+
+
+#[bench]
+fn extract_int_extract_fail(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let d = py.None().into_ref(py);
+
+        bench.iter(|| {
+            match black_box(d).extract::<i64>() {
+                Ok(v) => panic!("should err {}", v),
+                Err(e) => black_box(e),
+            }
+        });
+    });
+}
+
+#[bench]
+fn extract_int_downcast_success(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let int_obj: PyObject = 123.into_py(py);
+        let int = int_obj.as_ref(py);
+
+        bench.iter(|| {
+            let py_int = black_box(int).downcast::<PyInt>().unwrap();
+            let v = py_int.extract::<i64>().unwrap();
+            black_box(v);
+        });
+    });
+}
+
+
+#[bench]
+fn extract_int_downcast_fail(bench: &mut Bencher) {
+    Python::with_gil(|py| {
+        let d = py.None().into_ref(py);
+
+        bench.iter(|| {
+            match black_box(d).downcast::<PyInt>() {
+                Ok(v) => panic!("should err {}", v),
+                Err(e) => black_box(e),
+            }
+        });
+    });
+}
